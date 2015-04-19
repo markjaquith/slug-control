c9e2a1c6bfa1ad661f5c334ecb47a77329d63576
Mark Jaquith <mark@jaquith.me>
2015-04-19 14:44:00 -0400

    PHPDocs

--- classes/plugin.php
+++ classes/plugin.php
@@ -33,6 +33,12 @@ class CWS_Slug_Control_Plugin extends WP_Stack_Plugin2 {
 		$this->load_textdomain( 'cws-slug-control', '/languages' );
 	}
 
+	/**
+	 * Expands English language contractions to avoid ambiguity
+	 *
+	 * @param string $title The post title
+	 * @return string The modified post title
+	 */
 	public function uncontraction( $title ) {
 		$apos = "['’]";
 		$contractions = array(
@@ -51,17 +57,36 @@ class CWS_Slug_Control_Plugin extends WP_Stack_Plugin2 {
 		return $title;
 	}
 
+	/**
+	 * Spells out percentages explicitly. 10% => 10 percent
+	 *
+	 * @param string $title The post title
+	 * @return string The modified post title
+	 */
 	public function percentify( $title ) {
 		return preg_replace( '#\b(\d+)%(\s|$)#', '$1 percent$2', $title );
 	}
 
+	/**
+	 * Remove prepended phrased like "Breaking news:"
+	 *
+	 * @param string $title The post title
+	 * @return string The modified post title
+	 */
 	public function unprependify( $title ) {
 		return preg_replace( '#^((developing|breaking)( news)?|update(s|d)?):\s#i', '', $title );
 	}
 
+	/**
+	 * Changes ranges like 0-60 to "0 to 60"
+	 *
+	 * @param string $title The post title
+	 * @return string The modified post title
+	 */
 	public function rangerific( $title ) {
 		return preg_replace( '#(\s|^)(\d+)([-–—]+)(\d+)(\s|$)#', '$1$2 to $4$5', $title );
 	}
+
 	/**
 	 * Callback for sanitize_title filter
 	 *
